260291aa7e19a226c7ce3d0ddda353c53c1529a4
Raynel Sanchez <87539502+raynelfss@users.noreply.github.com>
2023-10-05 21:18:55 -0400

    Chore: Removing unused react logo.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 /* eslint "@typescript-eslint/explicit-function-return-type": "off" */
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 import { ChakraProvider } from '@chakra-ui/react'
 
@@ -9,18 +8,7 @@ function App () {
     <ChakraProvider>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>[Insert future <code>Colleague</code> app here]</h1>
         </header>
       </div>
     </ChakraProvider>
