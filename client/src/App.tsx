e521086b9aae91917667142b4090d8e4d8554d8b
Raynel Sanchez <87539502+raynelfss@users.noreply.github.com>
2023-12-25 20:56:16 -0500

    Test: Add ESLint and Prettier CI Job (#10)
    * Initial: Add CI ESLint Job
    
    * Test: Switch to pnpm, change requirements
    
    * Test: Revert to npm
    
    * Test: Test with CD
    
    * Test: Add eslint-standard-with-typescript
    
    * Test: Added the right pattern
    
    * Test: Check whether cd works
    
    * Test: Remove unnecessary steps
    
    * Test: Removed backwards slash
    
    * Test: Add extensions
    
    * Test: Adding changes to make test pass
    
    * Test: Try with pnpm/action-setup
    
    * Test: Try with run-install command
    
    * Test: Only use in PR, remove global install
    
    * Test: Remove install step
    
    * Test: Added typescript, kept global dependencies
    
    * Test: Remove global
    
    * Test: Added version 8
    
    * Test: Check node version
    
    * Test: Check Node Version
    
    * Test: Install using typescript-eslint method
    
    * Test: Re-organize args
    
    * Test: Use working directory
    
    * Test: Add prettier
    
    * Test: Fix prettier typo
    
    * Chore: Adding Prettier as a dependency
    
    * Test: Added non-conflicting rules between prettier and eslint
    
    * Test: Performed modifications with prettier
    
    * Test: Add Airbnb requirements to workflow
    
    * Test: Add .prettierIgnore file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,31 @@
+/* eslint-disable react/jsx-filename-extension */
 /* eslint "@typescript-eslint/explicit-function-return-type": "off" */
-import React from 'react'
-import './App.css'
-import { ChakraProvider, Text, Code } from '@chakra-ui/react'
+import React from "react";
+import "./App.css";
+import { ChakraProvider, Text, Code } from "@chakra-ui/react";
 
-function App () {
+function App() {
   return (
     <ChakraProvider>
       <div className="App">
         <header className="App-header">
           <Text>
             [Insert future
-            <Code bg={'gray.900'} fontSize='' fontWeight='bold' color='rgb(154, 87, 205)'> Colleague </Code>
+            <Code
+              bg="gray.900"
+              fontSize=""
+              fontWeight="bold"
+              color="rgb(154, 87, 205)"
+            >
+              {" "}
+              Colleague{" "}
+            </Code>
             app here]
           </Text>
         </header>
       </div>
     </ChakraProvider>
-  )
+  );
 }
 
-export default App
+export default App;
